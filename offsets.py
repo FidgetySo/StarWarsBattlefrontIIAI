9f49035048b32129534d2322607ff936d165ef7a
Elijah V. Schrotenboer <fidgetybog@gmail.com>
2022-07-28 20:41:04 -0400

    Add files via upload

---
+++ offsets.py
@@ -0,0 +1,16 @@
+class Offsets:
+    GameRenderer = 0x143FFBE10
+    RenderView = 0x538
+    ViewProj = 0x430
+    ClientGameContext = 0x143DD7948
+    PlayerManager = 0x58
+    LocalPlayer = 0x568
+    ClientArray = 0x768
+    Team = 0x58
+    ControlledControllable = 0x210
+    HealthComponent = 0x2C8
+    Height = 0x470
+    SoldierPrediction = 0x758
+    Occluded = 0xA58
+    Health = 0x20
+    Position = 0x20
